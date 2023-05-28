5d6754abf798cc5c793a2e343a294a4e43b3d05d
Pablo González de la Parra <a01745096@itesm.mx>
2023-05-27 23:20:29 -0600

    Host

--- admin/settings.py
+++ admin/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = \
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list = ['54.174.248.132', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS: list = ['54.162.170.59', '54.174.248.132', 'localhost', '127.0.0.1']
 
 
 # Application definition
